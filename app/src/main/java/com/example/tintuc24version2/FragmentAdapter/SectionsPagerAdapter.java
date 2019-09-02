da11e2d203b4dfff7128c0c2b512bf40b7a24bcf
N1_14_ChineseChess <45044050+Nhomltm03@users.noreply.github.com>
2019-09-03 04:42:51 +0700

    " Module weather "

--- app/src/main/java/com/example/tintuc24version2/FragmentAdapter/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/tintuc24version2/FragmentAdapter/SectionsPagerAdapter.java
@@ -8,24 +8,26 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-import com.example.tintuc24version2.NewsFragment;
+
+import com.example.tintuc24version2.NextDayWeatherFragment;
 import com.example.tintuc24version2.R;
+import com.example.tintuc24version2.TodayWeatherFragment;
 import com.example.tintuc24version2.WeatherFragment;
 
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.title_home, R.string.title_dashboard};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
     private final Context mContext;
-    private NewsFragment mNewsFragment;
-    private WeatherFragment mWeatherFragment;
+    private WeatherFragment mTodayWeatherFragment;
+    private NextDayWeatherFragment mNextDayWeatherFragment;
 
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
         super(fm);
         mContext = context;
-        mNewsFragment = new NewsFragment();
-        mWeatherFragment = new WeatherFragment();
+        mTodayWeatherFragment = new WeatherFragment();
+        mNextDayWeatherFragment = new NextDayWeatherFragment();
     }
 
     @Override
@@ -33,9 +35,9 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
         if(position == 0 ){
-            return  mNewsFragment;
+            return  mTodayWeatherFragment;
         } else if (position == 1 ){
-            return  mWeatherFragment;
+            return  mNextDayWeatherFragment;
         } else {
             //ToDO Something
         }
--- app/src/main/java/com/example/tintuc24version2/WeatherFragment.java
+++ app/src/main/java/com/example/tintuc24version2/WeatherFragment.java
@@ -1,19 +1,29 @@
 package com.example.tintuc24version2;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.tintuc24version2.FragmentAdapter.SectionsPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WeatherFragment extends Fragment {
+    private int[] tabIcons = {
+            R.drawable.ic_notifications_black_24dp,
+            R.drawable.ic_date
+    };
 
 
     public WeatherFragment() {
@@ -25,8 +35,19 @@ public class WeatherFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_news, container, false);
 
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter((Activity)getActivity(), getFragmentManager() );
+        ViewPager viewPager = rootView.findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs =rootView.findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        tabs.getTabAt(0).setIcon(tabIcons[0]);
+        tabs.getTabAt(1).setIcon(tabIcons[1]);
+
+        return rootView;
     }
 
 }
